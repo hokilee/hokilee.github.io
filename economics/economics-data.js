bb10c378f6312c36dafc39892601029dce612855
hokilee <modic_kr@naver.com>
2025-09-05 08:49:21 +0900

    게시판 업데이트 및 초기화면 수정 250905

--- economics/economics-data.js
+++ economics/economics-data.js
@@ -1,6 +1,16 @@
 // 경제상식 게시판 데이터
 const economicsData = [
   {
+    id: 61,
+    title: '리세션 (Recession)',
+    link: 'economics/economy-detail-61.html',
+    category: '경제 상식',
+    author: '이호기',
+    date: '09-05',
+    views: 0,
+    summary: '리세션 (Recession)의 개념, 특징, 비교 분석에 대한 설명',
+  },
+  {
     id: 60,
     title: '할로윈 효과(Halloween Effect)',
     link: 'economics/economy-detail-60.html',
