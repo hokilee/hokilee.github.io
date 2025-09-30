c406132d88ef795581f49ea8df0a3ca2324c896c
Hoki Lee <modic_kr@naver.com>
2025-09-30 10:26:52 +0900

    게시판 데이터 추가

--- one-line-knowledge/one-line-knowledge-data.js
+++ one-line-knowledge/one-line-knowledge-data.js
@@ -1,6 +1,15 @@
 // 한줄 지식/잡학 게시판 데이터
 const oneLineKnowledgeData = [
   {
+    id: 83,
+    title: '카카오톡의 개편과 소비자 반응',
+    link: 'one-line-knowledge/trivia-detail-83.html',
+    category: '한줄 지식/잡학',
+    author: '이호기',
+    date: '09-30',
+    views: 0,
+  },
+  {
     id: 82,
     title: 'AI 알고리즘 이란?',
     link: 'one-line-knowledge/trivia-detail-82.html',
